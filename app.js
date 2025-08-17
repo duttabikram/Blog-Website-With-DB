193bcd738248843d02e4336d5489ae246db4057f
duttabikram <136303237+duttabikram@users.noreply.github.com>
2025-08-17 14:36:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const homeStartingContent = "Discover a world of insights, stories, and experiences. Our blog is dedicated to sharing thought-provoking articles, personal reflections, and useful tips that inspire and inform. Whether you’re looking for lifestyle advice, travel tips, or the latest in technology, you’ll find something to spark your interest here.";
 const aboutContent = "At this website, we believe in the power of storytelling. Our mission is to create a platform where voices can be heard and ideas can flourish. Founded in [Year], our blog has grown from a personal diary into a community-driven space filled with diverse content.Our team consists of passionate writers and creators who are committed to delivering high-quality articles that resonate with our audience. We cover a range of topics, including lifestyle, travel, health, and technology, all aimed at enhancing your daily life.";
@@ -16,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
   title: String,
@@ -76,6 +77,6 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started successfully");
 });
