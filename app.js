6e8fc6099640461c0937739179e1cee4ddddc6f8
Bikram Dutta <duttabikram53@gmail.com>
2025-12-16 23:28:43 +0530

    Fix auth middleware and models, add login/signup

--- app.js
+++ app.js
@@ -190,6 +190,8 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
