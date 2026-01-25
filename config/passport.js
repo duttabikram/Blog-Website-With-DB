8290ba45b50e46ca1dfece22a34797a804c903b8
duttabikram <136303237+duttabikram@users.noreply.github.com>
2026-01-25 21:40:27 +0530

    Add files via upload

---
+++ config/passport.js
@@ -0,0 +1,57 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+
+// Google Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
+
+        let user = await User.findOne({ email });
+
+        // Existing user
+        if (user) {
+          if (!user.googleId) {
+            user.googleId = profile.id;
+            user.provider = "google";
+            await user.save();
+          }
+          return done(null, user);
+        }
+
+        // New user
+        user = await User.create({
+          email,
+          googleId: profile.id,
+          name: profile.displayName,
+          provider: "google"
+        });
+
+        done(null, user);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+);
+
+// SESSION SERIALIZATION
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
