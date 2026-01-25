818fa3bfd0563156097f0293cecb569cfa77ed9e
duttabikram <136303237+duttabikram@users.noreply.github.com>
2026-01-25 21:46:58 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    password: {
-      type: String,
-      required: true
-    }
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,      // for local login
+  googleId: String,      // for Google login
+  name: String,
+  provider: {
+    type: String,
+    enum: ["local", "google"],
+    default: "local"
+  }
+});
 
 module.exports = mongoose.model("User", userSchema);
